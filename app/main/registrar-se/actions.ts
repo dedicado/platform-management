9f7ac487b6aad0ea3f84913f2dceca90ff2879c5
Izequias Silva <master@dedicado.digital>
2024-04-11 09:09:12 -0300

    new implementations

--- app/main/registrar-se/actions.ts
+++ app/main/registrar-se/actions.ts
@@ -1,25 +1,21 @@
 'use server'
 
 import { userRepositoryCreate } from '@/repositories/user/POST'
-import {
-  RegisterValidation,
-  RegisterValidationType,
-} from '@/validations/register'
+import { RegisterValidationType } from '@/validations/register'
 import { revalidatePath, revalidateTag } from 'next/cache'
 
 export const registerUser = async (
   inputs: RegisterValidationType,
 ): Promise<any> => {
-  try {
-    if (await RegisterValidation.parseAsync(inputs)) {
-      return await userRepositoryCreate({ ...inputs, profile: 'guest' }).then(
-        async () => {
-          revalidateTag('users')
-          revalidatePath('/')
-        },
-      )
-    }
-  } catch (error: any) {
-    return error?.message || 'ocorreu um erro inesperado'
-  }
+  const phone: string = inputs?.phoneCountry + inputs?.phone
+  return await userRepositoryCreate({
+    name: inputs?.name,
+    email: inputs?.email,
+    phone: phone,
+    password: inputs?.password,
+    profile: 'guest',
+  }).then(async () => {
+    revalidateTag('users')
+    revalidatePath('/')
+  })
 }
