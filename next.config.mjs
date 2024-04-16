2cb3cf05fac7cf7401cab281407427bc0dddebdf
Izequias Silva <master@dedicado.digital>
2024-04-16 11:11:53 -0300

    components don`t used removed

--- next.config.mjs
+++ next.config.mjs
@@ -20,9 +20,6 @@ const nextConfig = {
     MAPBOX_STYLES: process.env.MAPBOX_STYLES ?? '',
     PLATFORM_AWS_ACCESS_KEY: process.env.PLATFORM_AWS_ACCESS_KEY ?? '',
     PLATFORM_AWS_PRIVATE_KEY: process.env.PLATFORM_AWS_PRIVATE_KEY ?? '',
-    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY ?? '',
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY ?? '',
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET ?? '',
   },
   images: {
     dangerouslyAllowSVG: true,
