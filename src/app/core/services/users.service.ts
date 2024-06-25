7b8d248e0fd368ccc3ef16dc2abcd2904d5018b5
Izequias Silva <master@dedicado.digital>
2024-06-25 18:13:15 -0300

    update: refactored

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -7,27 +7,27 @@ import { User } from '../interfaces/user.interface'
   providedIn: 'root',
 })
 export class UsersService {
-  endpoint = environment.platformApiUrl
+  endpoint = environment.platformApiUrl + '/users'
 
   constructor(private readonly http: HttpClient) {}
 
   create(data: User) {
-    return this.http.post<User>(`${this.endpoint}/users`, data)
+    return this.http.post<User>(`${this.endpoint}`, data)
   }
 
   findAll() {
-    return this.http.get<User[]>(`${this.endpoint}/users`)
+    return this.http.get<User[]>(`${this.endpoint}`)
   }
 
   findOne(id: string) {
-    return this.http.get<User>(`${this.endpoint}/users/${id}`)
+    return this.http.get<User>(`${this.endpoint}/${id}`)
   }
 
   update(id: string, data: User) {
-    return this.http.patch<User>(`${this.endpoint}/users/${id}`, data)
+    return this.http.patch<User>(`${this.endpoint}/${id}`, data)
   }
 
   remove(id: string) {
-    return this.http.delete<string>(`${this.endpoint}/users/${id}`)
+    return this.http.delete<string>(`${this.endpoint}/${id}`)
   }
 }
